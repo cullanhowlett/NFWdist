2870b4cc0ff5db3e23ae7ddb094a8997de8a383f
Cullan Howlett <cullan.howlett@uwa.edu.au>
2018-05-24 11:28:28 +0800

    added python codes

---
+++ python/NFWdist.py
@@ -0,0 +1,75 @@
+import numpy as np
+from scipy import special
+
+r"""  The Standard Distribution Functions for the 3D NFW Profile
+
+      Density, distribution function, quantile function and random generation for the 3D NFW profile
+
+      Usage:
+      dnfw(x, con = 5, log = FALSE)
+      pnfw(q, con = 5, log.p = FALSE)
+      qnfw(p, con = 5, log.p = FALSE)
+      rnfw(n, con = 5)
+
+      dnfw gives the density, pnfw gives the distribution function, qnfw gives the quantile function, and rnfw generates random deviates.
+
+      Arguments:
+        x, q: array_like
+          Vector of quantiles. This is scaled such that x=R/Rvir for NFW. This means the PDF is only defined between 0 and 1.
+
+        p: array_like
+          Vector of probabilities
+
+        n: array_like
+          Number of observations. If n has the attribute 'len()', i.e., is not a scalar, the length is taken to be the number required.
+
+        con: scalar, optional
+          The NFW profile concentration parameter, where c=Rvir/Rs.
+
+        log, logp: logical, optional
+          if True, probabilities/densities p are returned as log(p).
+
+      Examples:
+        see test.py
+
+      Notes:
+        The novel part of this package is the general solution for the CDF inversion (i.e. qnfw). 
+        As far as I can see this has not been published anywhere, and it is a useful function for populating halos in something like an HOD.
+        This seems to work at least as efficiently as accept/reject, but it is ultimately much more elegant code in any case.
+
+      Author:
+      Cullan Howlett
+"""
+
+def pnfwunorm(q, con=5):
+  return np.log(1.0 + q*con)-(con*q)/(1.0 + con*q)
+
+def dnfw(x, con=5, log=False):
+  if (log):
+    d = np.log(con**2*x/((con*x+1.0)**2*(1.0/(con+1.0)+np.log(con+1.0)-1.0)))
+  else:
+    d = con**2*x/((con*x+1.0)**2*(1.0/(con+1.0)+np.log(con+1.0)-1.0))
+  if hasattr(x, '__len__'):
+    d[x>1]=0
+  else:
+    if (x > 1):
+      d = 0
+  return d
+
+def pnfw(q, con=5, logp=False):
+  if(logp):
+    p = np.log(pnfwunorm(q, con=con)/pnfwunorm(1, con=con))
+  else:
+    p = pnfwunorm(q, con=con)/pnfwunorm(1, con=con)
+  return p
+
+def qnfw(p, con=5, logp=False):
+  if (logp):
+    p = np.exp(p)
+  p *= pnfwunorm(1, con=con)
+  return (-(1.0/np.real(special.lambertw(-np.exp(-p-1))))-1)/con
+
+def rnfw(n, con=5):
+  if hasattr(n, '__len__'):
+    n=len(n)
+  return qnfw(np.random.rand(int(n)), con=con)
---
+++ python/test.py
@@ -0,0 +1,54 @@
+from NFWdist import *
+
+# Just a quick main routine to test things
+def main():
+
+  # These libraries are only required for testing
+  import time
+  import numpy as np
+  import matplotlib.pyplot as plt
+  from scipy import integrate
+
+  #Both the PDF (dnfw) integrated up to x, and CDF at q should be the same:
+  print "Checking CDF and integral of PDF, should be the same:"
+  for con in [1,5,10,20]:
+    print integrate.quad(dnfw, 0, 0.5, args=(con))[0], pnfw(0.5, con=con)
+
+  #Both the qnfw should be the inverse of pnfw
+  print "Checking qnfw inverts pnfw, should return input vector [1:9]/10"
+  for con in [1,5,10,20]:
+    print(qnfw(pnfw(np.arange(1,10)/10.0,con=con), con=con))
+
+  # First some simple timing tests
+  print "Timing tests in seconds"
+  print "Average of 1000 iterations of nsamples=10,000:"
+  start = time.time()
+  for i in range(1000):
+    np.random.randn(10000)
+  print "Uniform: ", (time.time()-start)/1000.0
+
+  for con in [1,5,10,20]:
+    start = time.time()
+    for i in range(1000):
+      rnfw(1e4,con=con)
+    print str(r'con = %d: ' % con), (time.time()-start)/1000.0
+
+  # Some plots of the analytic and randomly-drawn NFW PDF
+  print "Plotting random draws against PDF for nsamples=100,000:"
+  nsamples = 1e6
+  for con in [1,5,10,20]:
+
+      hist = np.histogram(rnfw(nsamples,con=con), bins=100, density=True)
+
+      fig = plt.figure(con)
+      ax1=fig.add_axes([0.13,0.13,0.82,0.82])
+      ax1.plot(hist[1][1:], hist[0], color='k', linewidth=1.5, ls='steps')
+      ax1.plot(np.linspace(0,1,1e3), dnfw(np.linspace(0,1,1e3),con=con), color='r', ls='-')
+      ax1.set_xlabel(r'$q$', fontsize=16)
+      ax1.set_ylabel(r'$\rho(q)$', fontsize=16)
+      ax1.text(0.8, 0.8, str(r'con = %d' % con), transform=ax1.transAxes, fontsize='16')
+
+  plt.show()
+
+if __name__ == "__main__":
+  main()
